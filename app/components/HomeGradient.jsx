d6b1f20eca7e6c9b032e1180fda15d0c44e0a991
Utkarsh Singh <utkarsh.singh6113@gmail.com>
2024-12-15 20:59:12 +0530

    fixed home page

--- app/components/HomeGradient.jsx
+++ app/components/HomeGradient.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const HomeGradient = () => {
   return (
     <div className='h-screen absolute top-0 left-0 w-full z-[-1] bg-black'>
-        <div className='bg-blue-600 h-[25rem] w-[50rem] rounded-full blur-[180px] aspect-square absolute right-[-15rem] top-40'>
+        <div className='bg-blue-600 h-[25rem] w-[50rem] rounded-full blur-[180px] aspect-square absolute right-[-15rem] top-[10rem]'>
         </div>
-        <div className='bg-blue-600 h-[25rem] w-[50rem] rounded-full blur-[180px] aspect-square absolute left-[-10rem] bottom-[-50]'>
+        <div className='bg-blue-600 h-[25rem] w-[50rem] rounded-full blur-[180px] aspect-square absolute left-[-10rem] bottom-[-5rem]'>
         </div>
     </div>
   )
